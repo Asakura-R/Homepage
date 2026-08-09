a5e2ed8f776bd0fb127cea9a18017e7a6b5e6953
Asakura-R <ichihanov@gmail.com>
2026-08-09 21:36:54 +0900

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://asakura.example.com', // 本番URL
-});
